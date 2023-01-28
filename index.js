ddf353c4fc3e4b69c6de54cd4e3ab37e44dbdc4d
WagnerSkull <wagnerskull@hotmail.com>
2023-01-28 10:08:06 -0300

    Changes

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // Sempre que for enviar pro GITHub, apagar node_modules, após 
 //só instalar novamente usando os comando npm i, isso ira instalar os que foi apagado.
 //Pra iniciar o node , node index.js 
+//Iniciar servidor (npm run dev)
 
 //GET  => Buscar informação no back-end
 //POST => Criar informação no back-end
 //PUT / PATCH => Alterar/Atualizar informação no back-end
 //DELETE => Deletar informação no back-end
 
+//QUERY params => meusite.com/users?nome=rodolfo&age=28 // FILTROS DE PESQUISA 
+//ROUTE params => /users/2 // BUSCAR, DELETAR OU ATUALIZAR ALGO ESPECÌFICO
+//REQUEST BODY => {'name': 'Rodolfo', 'age':}
 
 //Ativando o express
 const express = require("express")
@@ -23,32 +27,28 @@ const app = express()
 //Falando pro express que vamos usar como padrão o express. 
 app.use(express.json())
 
-//QUERY params => meusite.com/users?nome=rodolfo&age=28 // FILTROS DE PESQUISA 
-//ROUTE params => /users/2 // BUSCAR, DELETAR OU ATUALIZAR ALGO ESPECÌFICO
-//REQUEST BODY => {'name': 'Rodolfo', 'age':}
 
 const users = []
 
 
-
 //Criando rota do express, Request requisitando, reponse respondendo.
+//Meu app GET mostra todos meus usuarios (USERS)
 app.get('/users', (request, response) => {
 
     return response.json(users)
 })
 
-
 //Criando rota POST
+//Mostra os USERS
 app.post('/users', (request, response) => {
     const { name, age } = request.body
 
-     //objeto
+    //objeto
     const user = { id: uuid.v4(), name, age }
 
     users.push(user)
-    
-    return response.status(201).json(user)
 
+    return response.status(201).json(user)
 })
 
 
