6dd1504a66265d9dafcad42a3550b44596b8ea7d
WagnerSkull <wagnerskull@hotmail.com>
2023-01-28 08:02:13 -0300

    err400json

--- index.js
+++ index.js
@@ -2,14 +2,24 @@
 //só instalar novamente usando os comando npm i, isso ira instalar os que foi apagado.
 //Pra iniciar o node , node index.js 
 
+//GET  => Buscar informação no back-end
+//POST => Criar informação no back-end
+//PUT / PATCH => Alterar/Atualizar informação no back-end
+//DELETE => Deletar informação no back-end
+
+
 //Ativando o express
-const express = require('express')
+const express = require("express")
+
+//Variavel para salvar a biblioteca UUID
+const uuid = require("uuid")
 
 //Constante da porta pra quando alterar ficar a programada
 const port = 3000
 
 //na const do express e usado nome server ou app.
 const app = express()
+
 //Falando pro express que vamos usar como padrão o express. 
 app.use(express.json())
 
@@ -17,28 +27,36 @@ app.use(express.json())
 //ROUTE params => /users/2 // BUSCAR, DELETAR OU ATUALIZAR ALGO ESPECÌFICO
 //REQUEST BODY => {'name': 'Rodolfo', 'age':}
 
+const users = []
+
 
 
 //Criando rota do express, Request requisitando, reponse respondendo.
 app.get('/users', (request, response) => {
 
-    const { name, age } = request.body
-
-
-
-
-    //Respondendo com o json, criando um objeto pegando as variaveis acima. 
-    //Se a chave da constante for o mesmo do valor da pra econmiza codigo ficaria (return response.json({name, age})),iria ficar assim.
-    //return response.json({name: name, age: age})
-    return response.json({ name, age })
-
+    return response.json(users)
 })
 
+
+//Criando rota POST
+app.post('/users', (request, response) => {
+    const { name, age } = request.body
+
+     //objeto
+    const user = { id: uuid.v4(), name, age }
+
+    users.push(user)
+
+    return response.status(201).json(user)
+
+})
 
 
 //Nós podemos escolher o número da porta, Porem não são qualquer número, do numero 80 pra cima. 
 //Alguns números já são usado por alguns bancos de dados, e instalando ele já vem com uma porta especifica do computador
 //Função Expresse PORTA- Ela aceita o segundo paramentro pra quando servidor começar a rodar, da pra dicionar função. tipo mensagem
 app.listen(port, () => {
-    console.log(`GOGO Server starded on port ${port}`)
+    console.log("GOGO Server starded on port ${port}")
 })
+
+
